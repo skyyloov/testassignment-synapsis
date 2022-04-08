47b55fbd4618ae9ebb3cd5af22b3f947367ad399
skyyloov <sonyfadhil11@gmail.com>
2022-04-08 11:35:26 +0700

    commit lagi

--- pages/new.js
+++ pages/new.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import { Button, Form, FormInput, Loader, Container } from 'semantic-ui-react';
